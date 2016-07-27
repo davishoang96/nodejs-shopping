82f0a66f15f96ad83e10a0cc3b533f4875f96726
davishoang96 <davishoang96@gmail.com>
2016-07-27 13:11:10 +0700

    Level 6 - Create a UserSchema for Mongoose

---
+++ models/user.js
@@ -0,0 +1,44 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+
+
+var UserSchema = new Schema({
+  email: {type: String, unique: true, lowercase: true},
+  password: String,
+
+  profile: {
+    name: {type: String, default: ''},
+    avatar: {type: String, default: ''}
+  },
+
+  address: String,
+  history: [{
+    date: Date,
+    paid: {type: Number, default: 0},
+
+  }]
+
+})
+
+// Hash the password
+
+UserSchema.pre('save', function(next){
+  var user = this;
+  if(!user.isModified('password')) return next();
+  bcrypt.genSalt(10, function(err, salt){
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, null, function(err, hash){
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+//Compare hashed password with user input from keyboard
+UserSchema.methods.comparePass = function(password){
+  return bcrypt.compareSync(password, this.password);
+}
+
+module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-var express = require('express'),
-    mongoose = require('mongoose'),
-    ejs = require('ejs'),
-    engine = require('ejs-mate'),
-    morgan = require('morgan');
+var express = require('express');
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var ejs = require('ejs');
+var engine = require('ejs-mate');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
 
 var db_url = "localhost:27017/shopping";
+var User = require('./models/user');
 
 mongoose.connect(db_url,function(err){
-  if (err) throw err
+  if (err) throw err;
+  mongoose.Promise = global.Promise;
   console.log('Connected to database');
 });
 
@@ -16,6 +20,23 @@ var app = express();
 
 //Middleware
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/create', function(req, res, next){
+  var user = new User();
+  user.profile.name = req.body.name;
+  user.password = req.body.password;
+  user.email = req.body.email;
+  user.address = req.body.address;
+
+  user.save(function(err){
+    if (err) next(err);
+    res.json('Successfully created a user');
+  });
+});
+
+
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
@@ -30,7 +51,11 @@ app.get('/about', function(req, res){
   res.render('main/about');
 })
 
-const PORT = 8080;
+app.get('/signup', function(req, res){
+  res.render('main/signup');
+})
+
+const PORT = 3000;
 
 app.listen(PORT, function(err){
   if (err) throw err;
